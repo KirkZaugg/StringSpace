03b4ea6818236682b86cc7081811d1cca775a485
KirkZaugg <25kzaugg@go.dsdmail.net>
2024-12-05 14:50:39 +0000

    fin

--- main.cpp
+++ main.cpp
@@ -11,14 +11,21 @@ void parseByStream(std::string instring, char delim) {
 }
 
 void parseByStrFunct(std::string instring, std::string delim) {
-    std::string work;
-    std::size_t x = instring.find(delim, delim.size());
-    work = instring.substr(0, x);
-    std::cout << work << "\n";
+    while(instring.size() > 0) {
+        std::size_t x = instring.find(delim, 0);
+        if (x == std::string::npos) {
+            std::cout << instring << "\n";
+            return;
+        }
+        std::cout << instring.substr(0, x) << "\n";
+        instring.erase(0, x + delim.size());
+    }
 }
 
 int main() {
-    std::string hello = "Hello, World!";
+    
+    std::string hello = "Hello, World!, I know you, you know, but I don't, think you, know me";
     parseByStream(hello, 'l');
+    std::cout << "\n";
     parseByStrFunct(hello, ", ");
 }
